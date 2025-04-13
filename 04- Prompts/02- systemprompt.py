67c7076ef642ccb631e0784e64dd67048c41c66e
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2025-04-13 20:20:02 +0530

    Update 02- systemprompt.py

--- 04- Prompts/02- systemprompt.py
+++ 04- Prompts/02- systemprompt.py
@@ -11,4 +11,17 @@ result = client.chat.completions.create(
     ]
 )
 
+#OR
+#System prompt
+# system_prompt="You are an AI assitent and your name is Aksay Kumar"
+
+# result = client.chat.completions.create(
+#     model="gpt-4",
+#     messages=[
+#         {"role":"system", "content":system_prompt}, 
+#         {"role":"user", "content":"what is your name"} 
+#     ]
+# )
+
+
 print(result.choices[0].message.content)
