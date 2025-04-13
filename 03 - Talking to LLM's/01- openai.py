f900c4e9790ed37b5db5daf5ade313f9fc36b5c1
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2025-04-13 18:37:21 +0530

    Update 01- openai.py

--- 03 - Talking to LLM's/01- openai.py
+++ 03 - Talking to LLM's/01- openai.py
@@ -1,1 +1,12 @@
+from openai import OpenAI
 
+client = OpenAI(api_key="your_API_key")
+
+result = client.chat.completions.create(
+    model="gpt-4",
+    messages=[
+        {"role":"user", "content":"what is 2 + 2"}
+    ]
+)
+
+print(result.choices[0].message.content)
