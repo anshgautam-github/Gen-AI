5767775911adec190eb94f19a04b5208631ebbd2
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2025-04-15 15:55:15 +0530

    Create 03- agent_auto.py

---
+++ 05- Agent/03- agent_auto.py
@@ -0,0 +1,85 @@
+import json
+from openai import OpenAI
+
+client = OpenAI(api_key="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")
+
+
+def get_weather(city:str):
+    #TODO: Do an actual API call
+    print("🔨 Tool Called: get_weather", city)
+    return "31 deg celcius"
+
+
+avaiable_tools = {
+    "get_weather": {
+        "fn": get_weather,
+        "description": "Takes a city name as an input and returns the current weather for the city"
+    }
+}
+
+system_prompt = f"""
+    You are an helpfull AI Assistant who is specialized in resolving user query.
+    You work on start, plan, action, observe mode.
+    For the given user query and available tools, plan the step by step execution, based on the planning,
+    select the relevant tool from the available tool. and based on the tool selection you perform an action to call the tool.
+    Wait for the observation and based on the observation from the tool call resolve the user query.
+
+    Rules:
+    - Follow the Output JSON Format.
+    - Always perform one step at a time and wait for next input
+    - Carefully analyse the user query
+
+    Output JSON Format:
+    {{
+        "step": "string",
+        "content": "string",
+        "function": "The name of function if the step is action",
+        "input": "The input parameter for the function",
+    }}
+
+    Available Tools:
+    - get_weather: Takes a city name as an input and returns the current weather for the city
+    
+    Example:
+    User Query: What is the weather of new york?
+    Output: {{ "step": "plan", "content": "The user is interseted in weather data of new york" }}
+    Output: {{ "step": "plan", "content": "From the available tools I should call get_weather" }}
+    Output: {{ "step": "action", "function": "get_weather", "input": "new york" }}
+    Output: {{ "step": "observe", "output": "12 Degree Cel" }}
+    Output: {{ "step": "output", "content": "The weather for new york seems to be 12 degrees." }}
+"""
+
+messages = [
+    { "role": "system", "content": system_prompt }
+]
+while True:
+    user_query = input('> ')
+    messages.append({ "role": "user", "content": user_query })
+
+    while True:
+        response = client.chat.completions.create(
+            model="gpt-4o",
+            response_format={"type": "json_object"},
+            messages=messages
+        )
+
+        parsed_output = json.loads(response.choices[0].message.content)
+        messages.append({ "role": "assistant", "content": json.dumps(parsed_output) })
+
+        if parsed_output.get("step") == "plan":
+            print(f"🤖: {parsed_output.get('content')}")
+
+            continue
+        
+        if parsed_output.get("step") == "action":
+            tool_name = parsed_output.get("function")
+            tool_input = parsed_output.get("input")
+
+            if avaiable_tools.get(tool_name, False) != False:
+                output = avaiable_tools[tool_name].get("fn")(tool_input)
+                messages.append({ "role": "assistant", "content": json.dumps({ "step": "observe", "output":  output}) })
+                continue
+        
+        if parsed_output.get("step") == "output":
+            print(f"🤖: {parsed_output.get('content')}")
+            break
