69dc7e0293c959758c0983556c52053188823d79
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2025-05-27 15:42:33 +0530

    Update sample.py

--- 06- Running LLM's Locally/Using Hugging Face/With pipeline/sample.py
+++ 06- Running LLM's Locally/Using Hugging Face/With pipeline/sample.py
@@ -1,9 +1,26 @@
+#📦 Step 1: Install & Set Up 
+#--------------------------------------------------------------
+
+#Installs the Hugging Face transformers library which gives us access to pre-trained language models.
 !pip install transformers
+
+#This sets your Hugging Face token (so you can access restricted models like Gemma). You can get this token from your Hugging Face account.
 import os
 os.environ["HF_TOKEN"] = ""
+
+#Specifies which model you're going to use. "google/gemma-3-1b-it" is a Google-trained instruction-tuned version of their Gemma model.
 model_name = "google/gemma-3-1b-it"
+#So, now how do we run a model? first we do tokenisation-> user input ko tokenize, then put in transformer , then de-tokenize it.
+
+#🤖 Step 2: Load the Model & Tokenizer
+#--------------------------------------------------------------
+
 from transformers import AutoTokenizer
+#Loads a tokenizer that converts text like "Hello" into numbers (tokens) that the model understand
+#Differnt models have diff tokenizers, here, autoTokenizer automatically takes care which tokenizer to make use of as per the model 
 tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+#Shows how the tokenizer breaks the input sentence into token IDs + attention mask.
 print(tokenizer("Hello, how are you?"))
 print(tokenizer.get_vocab())
 input_tokens = tokenizer("Hello, how are you?")["input_ids"]
