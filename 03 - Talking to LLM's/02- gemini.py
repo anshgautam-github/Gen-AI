5962f704eea91822d15569e13833a7c897ee25f3
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2025-05-27 16:43:52 +0530

    Update 02- gemini.py

--- 03 - Talking to LLM's/02- gemini.py
+++ 03 - Talking to LLM's/02- gemini.py
@@ -4,7 +4,8 @@ from google.genai import types
 # Only run this block for Gemini Developer API
 client = genai.Client(api_key='your_API_key')
 
-response = client.models.generate_content( #here in the content,we do not need to specify the start,end, etc, this particualr sdk will do on it's own
+response = client.models.generate_content( 
+    #here in the content,we do not need to specify the start,end, etc, this particualr sdk will do on it's own
     model='gemini-2.0-flash-001', contents='Why is the sky blue?'
     
 )
