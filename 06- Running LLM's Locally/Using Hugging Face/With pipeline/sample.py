211acc6f175ae8b4eb1ba14dd81f4cb1e682d58f
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2025-05-27 15:58:41 +0530

    Update sample.py

--- 06- Running LLM's Locally/Using Hugging Face/With pipeline/sample.py
+++ 06- Running LLM's Locally/Using Hugging Face/With pipeline/sample.py
@@ -1,5 +1,6 @@
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------
 #📦 Step 1: Install & Set Up 
-#--------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 #Installs the Hugging Face transformers library which gives us access to pre-trained language models.
 !pip install transformers
@@ -12,8 +13,10 @@ os.environ["HF_TOKEN"] = ""
 model_name = "google/gemma-3-1b-it"
 #So, now how do we run a model? first we do tokenisation-> user input ko tokenize, then put in transformer , then de-tokenize it.
 
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------
 #🤖 Step 2: Load the Model & Tokenizer
-#--------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 from transformers import AutoTokenizer
 #Loads a tokenizer that converts text like "Hello" into numbers (tokens) that the model understand
@@ -25,8 +28,14 @@ print(tokenizer("Hello, how are you?"))
 print(tokenizer.get_vocab())
 input_tokens = tokenizer("Hello, how are you?")["input_ids"]  #yeh input_id -> when u print tokenizer thing, u get output, in that we get token as well as attention mask-> usmei se we only need input token , so we mentined there specifically
 print(input_tokens)
+#Now tokenization process done -> we now have to load the model 
 
 
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#🧠 Step 4: Load the Model
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#Loads the actual Gemma model in bfloat16 format (a lighter precision that saves memory, often used on Colab with GPUs).
 from transformers import AutoModelForCausalLM
 import torch
 
@@ -34,16 +43,26 @@ model = AutoModelForCausalLM.from_pretrained(
     model_name,
     torch_dtype=torch.bfloat16
 )
+#now we have both tokenizer and the model(tranformer), now we just have to start the transformer 
 
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#✨ Step 5: Generate a Response
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 #Create a Text Generation Pipeline
 from transformers import pipeline
-gen_pipeline = pipeline("text-generation", model=model, tokenizer=tokenizer)
 
+#Transformers have a function named pipeline, so we can create a pipeline for text generation, and we say to that pipeline, that i need a textgeneration piepline,
+#and i pass my model and tokenizer
+gen_pipeline = pipeline("text-generation", model=model, tokenizer=tokenizer)
+#pipleine crated 
+#So i said to my pipeline func -> that this is my tokenizer and model, so jo bhi user tumko input dega-> usko iss tokenizer se tokenize and detokenize krna h and
+#usi model ya transformer ke andar use krna h.
 
 #Generate Text
 gen_pipeline("Hey there", max_new_tokens=25)
-
+#now i just calleld my pipeline with the input and internally, it did create token, de tokentize..etc
 
 #The pipeline in the transformers library provides a high-level abstraction for working with transformer-based models. 
 #It simplifies the process of running common tasks like text generation, text classification, sentiment analysis, and more, without requiring you to write a lot of code for each task.
